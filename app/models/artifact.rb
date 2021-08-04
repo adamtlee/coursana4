393a68cefb15768584d7cb3f5ec408781ff86135
adam <adam.lee346@gmail.com>
2021-08-04 12:50:36 -0600

    in progress working artifact

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,3 +1,16 @@
 class Artifact < ActiveRecord::Base
+  attr_accessor :upload
   belongs_to :project
+
+  MAX_FILESIZE = 10.megabytes
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+
+  validate :uploaded_fize_size
+
+  def uploaded_fize_size
+    if upload 
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
 end
